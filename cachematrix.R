edfcc82bf46970e55e1fd1978fbf40ea8833826b
Wenkai Liang <wenkail@princeton.edu>
2014-09-18 17:23:24 -0400

    Update1

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
 
-## Write a short comment describing this function
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
